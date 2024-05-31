a089ebe2c527353ba136c03c395e5ddac86582f9
mr_impossibl <210927@sdmcujire.in>
2024-05-31 12:10:01 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='about' >
+      <h1>ABOUT US</h1>
+      <h6>
+Welcome to Our Website
+
+In our website, we believe that shopping should be an enjoyable and convenient experience. Founded in 2023, our mission is to provide a diverse range of high-quality products at competitive prices, all while ensuring top-notch customer service.
+</h6>
+<h4>Our Story</h4>
+ <h6>Our website was born out of a passion for "fashion and accessories," "home and garden," "technology and gadgets". We recognized a need for a more personalized and customer-centric shopping experience. With this vision in mind, we set out to create a platform where customers could easily find what they were looking for, and discover new and exciting products along the way.</h6>
+      <img src="https://cdn.shopify.com/s/files/1/0840/8370/3830/files/1603968734-studio-always-creative-1.jpg" width='1000px' height='200px'>
+
+      </img>
+      <h4>Our Commitment</h4>
+<h4>Customer satisfaction is at the heart of everything we do. We are committed to:</h4>
+
+<h6>Quality: Only offering products that we believe in and stand behind.</h6>
+<h6>Service: Providing exceptional customer service and support.</h6>
+<h6>Value: Ensuring competitive pricing on all our products.</h6>
+<h6>Sustainability: Implementing eco-friendly practices and sourcing sustainable products whenever possible.</h6>
+<h4>
+Why Shop With Us?</h4>
+<h6>When you shop with us, you can expect:</h6>
+
+<h6>Easy Navigation: A user-friendly website that makes shopping simple and enjoyable.</h6>
+<h6>Secure Payments: Safe and secure payment options to protect your information.</h6>
+<h6>Fast Shipping: Prompt and reliable delivery to get your items to you quickly.</h6>
+<h6>Hassle-Free Returns: Easy return policies to ensure you are completely satisfied with your purchase.</h6>
+
+    
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='pro'>
+      <h1>WELCOME TO OUR PAGE </h1>
+      <img src="https://png.pngtree.com/thumb_back/fh260/back_our/20190619/ourmid/pngtree-taobao-vector-creative-technology-online-shopping-illustration-computer-finger-poster-image_131803.jpg">
+        
+      </img>
+    </div>
+  );
+}
+
+export default Home;
